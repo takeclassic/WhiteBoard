9d827dfa615529859865e27c02306b615c7ba98f
takeclassic@naver.com <takeclassic@naver.com>
2022-07-20 00:10:09 +0900

    Remove App bar and add appbar like bottom buttons

--- app/src/main/java/com/thinkers/whiteboard/MainActivity.kt
+++ app/src/main/java/com/thinkers/whiteboard/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.thinkers.whiteboard
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.thinkers.whiteboard.common.interfaces.IMemoInfoReceiver
 import com.thinkers.whiteboard.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity(), IMemoInfoReceiver {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     private var memoId: Int = -1
+
+    private val appBarMenuButtonClickListener = View.OnClickListener {
+        if (!binding.drawerLayout.isDrawerOpen(Gravity.START)) {
+            binding.drawerLayout.openDrawer(Gravity.START)
+        }
+    }
+
+    private val appBarSearchButtonClickListener = View.OnClickListener {
+
+    }
+
+    private val appBarFavoriteButtonClickListener = View.OnClickListener {
+        navController.navigate(R.id.nav_favorites)
+    }
+
+    private val appBarWriteButtonClickListener = View.OnClickListener {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +45,14 @@ class MainActivity : AppCompatActivity(), IMemoInfoReceiver {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_total, R.id.nav_favorites, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
         navView.setupWithNavController(navController)
+
+        binding.appbarMenuButton.setOnClickListener(appBarMenuButtonClickListener)
+        binding.appbarSearchButton.setOnClickListener(appBarSearchButtonClickListener)
+        binding.appbarFavoritesButton.setOnClickListener(appBarFavoriteButtonClickListener)
+        binding.appbarWriteButton.setOnClickListener(appBarWriteButtonClickListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,11 +62,33 @@ class MainActivity : AppCompatActivity(), IMemoInfoReceiver {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        //val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun setMemoId(id: Int) {
         memoId = id
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerVisible(Gravity.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            if (navController.currentDestination == null) {
+                Log.i(TAG, "current destination is null")
+            }
+
+            Log.i(TAG, "id: ${navController.currentDestination?.id}")
+
+            if (navController.currentDestination?.id == R.id.nav_favorites) {
+                Log.i(TAG, "it is favorites")
+                finish()
+            }
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        val TAG = "MainActivity"
     }
 }
