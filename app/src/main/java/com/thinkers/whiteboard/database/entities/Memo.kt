45d145352535219e5c2a79b0b29a00d40e5493ce
takeclassic@naver.com <takeclassic@naver.com>
2022-08-15 15:29:40 +0900

    Add Foreign key to memo table

--- app/src/main/java/com/thinkers/whiteboard/database/entities/Memo.kt
+++ app/src/main/java/com/thinkers/whiteboard/database/entities/Memo.kt
@@ -1,16 +1,35 @@
 package com.thinkers.whiteboard.database.entities
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE,
+            entity = Note::class,
+            parentColumns = ["note_name"],
+            childColumns = ["note_name"]
+        )
+    ],
+    indices = [
+        Index("note_name")
+    ]
+)
 data class Memo(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "memo_id") val memoId: Int,
-    @ColumnInfo(name = "title") var title: String?,
-    @ColumnInfo(name = "text") var text: String,
-    @ColumnInfo(name = "created_time") val createdTime: Long,
-    @ColumnInfo(name = "revised_time") var revisedTime: Long?,
-    @ColumnInfo(name = "note_name") var noteName: String,
-    @ColumnInfo(name = "is_favorite") var isFavorite: Boolean = false
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "memo_id")
+    val memoId: Int,
+    @ColumnInfo(name = "title")
+    var title: String?,
+    @ColumnInfo(name = "text")
+    var text: String,
+    @ColumnInfo(name = "created_time")
+    val createdTime: Long,
+    @ColumnInfo(name = "revised_time")
+    var revisedTime: Long?,
+    @ColumnInfo(name = "note_name")
+    var noteName: String,
+    @ColumnInfo(name = "is_favorite")
+    var isFavorite: Boolean = false
 )
