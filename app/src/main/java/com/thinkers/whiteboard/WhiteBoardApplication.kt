d80c16a9d452ce713c93c0128589dc69a3a76984
takeclassic <takeclassic@naver,com>
2023-07-06 23:13:01 +0900

    Add passcode encryption logic

--- app/src/main/java/com/thinkers/whiteboard/WhiteBoardApplication.kt
+++ app/src/main/java/com/thinkers/whiteboard/WhiteBoardApplication.kt
@@ -2,20 +2,30 @@ package com.thinkers.whiteboard
 
 import android.app.Application
 import android.content.Context
+import android.security.KeyPairGeneratorSpec
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.util.Log
-import androidx.room.Room
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import com.thinkers.whiteboard.common.utils.DispatcherProviderUtil
 import com.thinkers.whiteboard.common.notifications.NotificationHelper
+import com.thinkers.whiteboard.common.utils.DataStoreHelper
+import com.thinkers.whiteboard.common.utils.DispatcherProviderUtil
 import com.thinkers.whiteboard.database.AppDatabase
 import com.thinkers.whiteboard.database.repositories.MemoRepository
 import com.thinkers.whiteboard.database.repositories.NoteRepository
+import com.thinkers.whiteboard.database.repositories.SettingRepository
 import com.thinkers.whiteboard.settings.AutoRemoveWorker
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
+import java.security.KeyPairGenerator
+import java.security.KeyStore
 import java.util.concurrent.TimeUnit
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+
 
 class WhiteBoardApplication: Application() {
     init {
@@ -35,11 +45,23 @@ class WhiteBoardApplication: Application() {
             DispatcherProviderUtil()
         )
     }
+    val settingRepository by lazy {
+        SettingRepository(
+            database.settingDao(),
+            DispatcherProviderUtil()
+        )
+    }
+
+    private val dataStore: DataStore<Preferences> by preferencesDataStore(name = "whiteboard_prefs")
+    val dataStoreHelper by lazy {
+        DataStoreHelper(dataStore)
+    }
 
     override fun onCreate() {
         super.onCreate()
         NotificationHelper.createNotificationChannel(this)
         startAutoRemove()
+        createKey()
     }
 
     fun startAutoRemove() {
@@ -66,6 +88,33 @@ class WhiteBoardApplication: Application() {
         }
     }
 
+    private fun createKey() {
+        runCatching {
+            val keyStore: KeyStore = KeyStore.getInstance(AndroidKeyStore)
+            keyStore.load(null)
+            val secretKey: SecretKey = keyStore.getKey(KEY_NAME, null) as SecretKey
+            Log.i(TAG, "secretKey :$secretKey")
+        }.onSuccess {
+            Log.i(TAG, "succeed to get the key")
+        }.onFailure {
+            Log.i(TAG, "failed to create key: ${it.message}")
+            it.printStackTrace()
+
+            val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, AndroidKeyStore)
+            keyGenerator.init(
+                KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build()
+            )
+            keyGenerator.generateKey()
+        }
+    }
+
+    fun getIv() {
+
+    }
+
     companion object {
         var instance: WhiteBoardApplication? = null
         fun context(): Context {
@@ -74,5 +123,7 @@ class WhiteBoardApplication: Application() {
 
         const val TAG = "WhiteBoardApplication"
         const val WORK_NAME = "auto_remove"
+        const val KEY_NAME = "whiteboard_key"
+        const val AndroidKeyStore = "AndroidKeyStore"
     }
 }
--- app/src/main/java/com/thinkers/whiteboard/settings/LockViewModel.kt
+++ app/src/main/java/com/thinkers/whiteboard/settings/LockViewModel.kt
@@ -1,7 +1,19 @@
 package com.thinkers.whiteboard.settings
 
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import android.util.Log
 import android.widget.ImageView
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.thinkers.whiteboard.WhiteBoardApplication
+import com.thinkers.whiteboard.common.utils.DataStoreKeys
+import com.thinkers.whiteboard.database.entities.Setting
+import kotlinx.coroutines.launch
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+
 
 class LockViewModel: ViewModel() {
     var passcode = StringBuilder()
@@ -12,5 +24,34 @@ class LockViewModel: ViewModel() {
         passcode.clear()
         lockNumbers.clear()
         lockTinted = Array(4) { false }
+
+        encryptPasscode()
+    }
+
+    fun encryptPasscode() {
+        val keyStore: KeyStore = KeyStore.getInstance(WhiteBoardApplication.AndroidKeyStore)
+        keyStore.load(null)
+        val secretKey: SecretKey = keyStore.getKey(WhiteBoardApplication.KEY_NAME, null) as SecretKey
+        val transformation = KeyProperties.KEY_ALGORITHM_AES
+            .plus("/")
+            .plus(KeyProperties.BLOCK_MODE_CBC)
+            .plus("/")
+            .plus(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+
+        val cipher: Cipher = Cipher.getInstance(transformation)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        val iv = cipher.iv
+        val encodedIv: String = Base64.encodeToString(iv, Base64.NO_WRAP)
+        viewModelScope.launch {
+            WhiteBoardApplication.instance!!.dataStoreHelper.storeStringValue(DataStoreKeys.STRING_KEY_IV, encodedIv)
+        }
+
+        val encrypted = cipher.doFinal(passcode.toString().toByteArray())
+        val setting = Setting("passcode", encrypted.toString(Charsets.UTF_8))
+        WhiteBoardApplication.instance!!.settingRepository.setSetting(setting)
+    }
+
+    fun decryptPasscode() {
+
     }
 }
